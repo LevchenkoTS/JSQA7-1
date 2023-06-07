5183cb0c9dfd02b4bfc2b255f4d55837c4382a8a
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2023-06-07 22:49:34 +0300

    hw7.1

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectId: '2y21zj',
+  e2e: {
+    baseUrl: 'http://qamid.tmweb.ru',
+    projectId: '2y21zj',
+    setupNodeEvents(on, config) {
+       
+      // implement node event listeners here
+    },
+  },
+};
